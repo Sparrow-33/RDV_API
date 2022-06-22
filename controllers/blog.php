5f13c6a30f8d43bbba8776d4c165003f13befa7e
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-06-22 11:57:56 +0100

    blog backend updated

--- controllers/blog.php
+++ controllers/blog.php
@@ -1,14 +1,125 @@
 <?php
 
-    class blog extends Controller
+    class Blog extends Controller
     {
         public function __construct()
         {
-            $this->articleModel = $this->model('blogModel');
+            $this->articleModel = $this->model('BlogModel');
+        }
+
+     //bring 3 articles from the database
+        public function getThreeArticles()
+        {
+            $result = json_decode(file_get_contents('php://input'));
+
+            $id = $result->id;
+            $blog_id = $result->blog_id;
+
+
+            $articles = $this->articleModel->getThreeArticles($id, $blog_id);
+
+            if($articles){
+                echo json_encode($articles);
+            } else {
+                echo json_encode(['error' => 'No articles found']);
+            }
+
+
+            
+        }
+
+        //function to like an article
+        public function likeArticle()
+        {
+            $result = json_decode(file_get_contents('php://input'));
+
+            $id = $result->id;
+            $blog_id = $result->blog_id;
+
+            $article = $this->articleModel->likeArticle($id, $blog_id);
+
+            if($article)
+            {
+                echo json_encode(['success' => true]);
+            } else
+            {
+                echo json_encode(['error' => false]);
+            }
+        }
+
+        //function to unlike an article
+        public function unlikeArticle()
+        {
+            $result = json_decode(file_get_contents('php://input'));
+
+            $id = $result->id;
+            $blog_id = $result->blog_id;
+
+            $article = $this->articleModel->unlikeArticle($id, $blog_id);
+
+            if($article)
+            {
+                echo json_encode(['success' => true]);
+            } else
+            {
+                echo json_encode(['error' => false]);
+            }
+        }
+
+        // already liked
+        public function alreadyLiked()
+        {
+            $result = json_decode(file_get_contents('php://input'));
+
+            $id = $result->id;
+            $blog_id = $result->blog_id;
+
+            $article = $this->articleModel->alreadyLiked($id, $blog_id);
+
+            if($article)
+            {
+                echo json_encode(['message' => true]);
+            } else
+            {
+                echo json_encode(['message' => false]);
+            }
+        }
+
+        //function get number of likes
+        public function getNumberOfLikes()
+        {
+            $result = json_decode(file_get_contents('php://input'));
+
+            $blog_id = $result->blog_id;
+
+            $num = $this->articleModel->getNumberOfLikes($blog_id);
+
+            if($num)
+            {
+                echo json_encode(['message' => $num]);
+            } else
+            {
+                echo json_encode(['message' => $num]);
+            }
+        }
+        
+        //function to get all articles
+
+        public function getAllArticles(){
+
+            $articles = $this->articleModel->getAllArticles();
+
+            if($articles){
+                echo json_encode($articles);
+            } else {
+                echo json_encode(['message' => false]);
+            }
+
+
         }
 
 
-        
+
 
     }
 
--- models/blogModel.php
+++ models/blogModel.php
@@ -1,10 +1,105 @@
 <?php
 
-class User {
+class BlogModel {
     private $db;
 
     public function __construct(){
       $this->db = new Database;
+    }
+
+    public function getThreeArticles($id, $blog_id){
+      $this->db->query('SELECT * FROM article WHERE cat_ID = :id AND NOT ID = :blog_id LIMIT 3');
+      $this->db->bind(':id', $id);
+      $this->db->bind(':blog_id', $blog_id);
+      $result = $this->db->resultSet();
+
+      if( $result){
+        return $result;
+      } else {
+        return false;
+      }
+
+    }
+
+    //function to like an article
+    public function likeArticle($id, $blog_id){
+
+      $this->db->query('INSERT INTO likes (UID, AID)  VALUES( :id , :blog_id )');
+      $this->db->bind(':id', $id);
+      $this->db->bind(':blog_id', $blog_id);
+      $result = $this->db->execute();
+
+      if( $result){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    //function to unlike an article
+    public function unlikeArticle($id, $blog_id){
+
+      $this->db->query('DELETE FROM likes WHERE UID = :id AND AID = :blog_id');
+      $this->db->bind(':id', $id);
+      $this->db->bind(':blog_id', $blog_id);
+      $result = $this->db->execute();
+
+      if( $result){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    // already liked
+    public function alreadyLiked($id, $blog_id){
+      $this->db->query('SELECT * FROM likes WHERE UID = :id AND AID = :blog_id');
+      $this->db->bind(':id', $id);
+      $this->db->bind(':blog_id', $blog_id);
+
+      $this->db->resultSet();
+      $num = $this->db->rowCount();
+
+      if($num > 0){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    //function to get number of likes
+    public function getNumberOfLikes($blog_id){
+      $this->db->query('SELECT * FROM likes WHERE AID = :blog_id');
+      $this->db->bind(':blog_id', $blog_id);
+      $this->db->resultSet();
+      $num = $this->db->rowCount();
+
+      
+
+      if( $num){
+        return $num;
+      } else {
+        return $num;
+      }
+    }
+
+    //function to get all articles
+
+    public function getAllArticles(){
+
+      $this->db->query('SELECT A.ID, A.title, A.cover, A.cat_ID, A.UID,A.time, C.IDC, C.CatName, U.name, U.profile
+                              FROM article A, user U, categories C
+                                   WHERE A.UID  = U.ID
+                                       AND A.cat_ID = C.IDC');
+
+      $result = $this->db->resultSet();
+
+      if( $result){
+        return $result;
+      } else {
+        return false;
+      }
+
     }
 
 }    
